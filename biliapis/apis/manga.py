5e91b1f21794d553355e896dfe7763698b68f80b
NingmengLemon <guanchenxiang@outlook.com>
2024-08-01 11:14:25 +0800

    add docstring for manga apis

--- biliapis/apis/manga.py
+++ biliapis/apis/manga.py
@@ -16,24 +16,31 @@ class MangaAPIs(template.APITemplate):
     @checker.check_bilicode(msgkey="msg")
     @template.request_template("post")
     def get_detail(self, mcid: int):
+        """获取一本漫画的详细信息"""
         return MangaAPIs.API_DETAIL + MangaAPIs.PARAMS_UNI, {"data": {"comic_id": mcid}}
 
     @utils.pick_data()
     @checker.check_bilicode(msgkey="msg")
     @template.request_template("post")
     def get_episode_info(self, epid: int):
+        """获取一个漫画章节的信息"""
         return MangaAPIs.API_EP_INFO + MangaAPIs.PARAMS_UNI, {"data": {"id": epid}}
 
     @utils.pick_data()
     @checker.check_bilicode(msgkey="msg")
     @template.request_template("post")
     def get_image_index(self, epid: int):
+        """获取一个漫画章节的图片目录，每张图片对应一个path用于获取图片本体"""
         return MangaAPIs.API_IMG_INDEX + MangaAPIs.PARAMS_UNI, {"data": {"ep_id": epid}}
 
     @utils.pick_data()
     @checker.check_bilicode(msgkey="msg")
     @template.request_template("post")
     def get_image_token(self, *paths: str):
+        """用图片path获取图片token，
+        将每张图片的url字段和token字段拼接可得到图片的最终url
+        
+        url+ "?token=" + token"""
         return MangaAPIs.API_IMG_TOKEN + MangaAPIs.PARAMS_UNI, {
             "data": {"urls": json.dumps(list(paths))}
         }
